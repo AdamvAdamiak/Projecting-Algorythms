49637d9ca88c9d49cbc68286985ed4481681dd02
Adam Adamiak 254355 <adamvadamiak@gmail.com>
2021-04-03 19:32:28 +0100

    end task 5

--- List 3/filtration.py
+++ List 3/filtration.py
@@ -1,4 +1,35 @@
 import numpy as np
 from math import sin
 from matplotlib import pyplot as plt
-from fourier_transformation import fft
+from fourier_transformation import fft, define_data
+from scipy import ifft
+
+
+def remove_frequency(data, freq=0.9):
+    for i in range(len(data)):
+        if data[i] > freq:
+            data[i] = 0
+    return data
+
+
+def show_plot():
+    plt.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+    data = define_data()
+    hamming = np.hamming(len(data))
+
+    data = data * hamming
+
+    data = fft(data)
+    plt.plot(data, color='orange', label='transformed data')
+
+    data = remove_frequency(data)
+    plt.plot(data, color='black', label='removed frequency')
+
+    data = ifft(data)
+    plt.plot(data, color='red', label='removed frequency, ifft')
+
+    show_plot()
