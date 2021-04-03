dd2aa29eb63a6b675c573453855720663312145f
Adam Adamiak 254355 <adamvadamiak@gmail.com>
2021-04-03 17:47:49 +0100

    end task 3

---
+++ List 3/propabilistic tests.py
@@ -0,0 +1,45 @@
+import random
+
+
+def fermat(p, k=40):
+    i = 0
+
+    while i < k:
+        a = random.randint(1, p - 1)
+        if pow(a, (p - 1), p) == 1:
+            i = i + 1
+        else:
+            return False
+
+    return True
+
+
+def miller_rabin(n, k=40):
+
+    if n == 2:
+        return True
+
+    if n % 2 == 0:
+        return False
+
+    r, s = 0, n - 1
+    while s % 2 == 0:
+        r += 1
+        s //= 2
+    for _ in range(k):
+        a = random.randrange(2, n - 1)
+        x = pow(a, s, n)
+        if x == 1 or x == n - 1:
+            continue
+        for _ in range(r - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
+
+
+print(fermat(5))
+
+print(miller_rabin(1621))
